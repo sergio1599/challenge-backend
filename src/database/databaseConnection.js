d94cdf0c60fe356469c79ec5f90070cc74b4202d
sergio1599 <sergioq15@hotmail.com>
2022-07-14 11:57:21 -0500

    changes in .env

--- src/database/databaseConnection.js
+++ src/database/databaseConnection.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const Sequelize = require('sequelize');
 const { ConnectionString } = require('connection-string');
-const cs = new ConnectionString(process.env.CONNECTION_URI);
+
+
+const cs = new ConnectionString(process.env.JAWSDB_URL);
 const { name } = cs.hosts?.[0];
-require('dotenv').config();
-
-
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.PASSWORD, {
     host: name,
     dialect: 'mysql',
