e445ef07d7f481309758f9f862ad866d5f6a7fda
sergio1599 <sergioq15@hotmail.com>
2022-07-14 09:43:04 -0500

    config the server

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
+      isGlobal: true,
     }),
   ],
   controllers: [AppController],
